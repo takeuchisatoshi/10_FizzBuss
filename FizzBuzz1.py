6e296bb9b8bf057343e9df9a4de35bb01b8aa8b0
takeuchisatoshi <satsitt3@gmail.com>
2019-03-10 08:54:34 +0900

    FizzBuzz2作成

---
+++ FizzBuzz1.py
@@ -0,0 +1,14 @@
+number = int(input(f"1つの自然数を入れてね > "))
+
+if number % 15 == 0:
+    print("FizzBuzz")
+
+elif number % 3 == 0:
+    print("Fizz")
+
+elif number % 5 == 0:
+    print("Buzz")
+
+else:
+    print(number)
+
