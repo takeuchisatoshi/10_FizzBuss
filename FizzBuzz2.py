0e17b8864411eb02aba5bbc0c716c8d5e9e52385
takeuchisatoshi <satsitt3@gmail.com>
2019-03-10 09:04:08 +0900

    FizzBuzz2完成

---
+++ FizzBuzz2.py
@@ -0,0 +1,16 @@
+print("1からNまでのFizzBuzz")
+
+number = int(input(f"1つの自然数を入れてね > "))
+
+for number in range(1, number + 1):
+    if number % 15 == 0:
+        print("FizzBuzz")
+
+    elif number % 3 == 0:
+        print("Fizz")
+
+    elif number % 5 == 0:
+        print("Buzz")
+
+    else:
+        print(number)
